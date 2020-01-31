136b13b4d1920aaf998e03e454f724e9f503cfd2
Stroganoff90 <salaevsergey@yandex.ru>
2020-01-31 15:47:01 +0400

    nice try(no)

--- src/index.jsx
+++ src/index.jsx
@@ -1,59 +1,84 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-// import MessageField from "./compons/MessageField/MessageField";
-// import SendButton from "./compons/SendButton/SendButton";
+// import MessageField from ".App/compons/MessageField/MessageField";
+// import SendButton from ".App/compons/SendButton/SendButton";
+// import InputField from ".App/compons/InputField/InputField";
+
+// componentDidMount() {
+//   console.log();
+// }
+
+const Message = ({ name, content }) => <div><strong>{name}:</strong> {content}</div>;
+
+const messages = [
+  {name: "Richard", content: "do you like hurting other people?"},
+];
 
 class MessageField extends React.Component {
+  componentDidMount() {
+    console.log('field');
+  }
+
+  // state() {
+  //   messages: messages
+  // }
+  
+  componentDidUpdate(messages) {
+    if (this.messages !== messages) {
+      this.setState(this.messages)
+    }
+  }
+
   render() {
     return messages.map((message, index) => <Message {...message} key={index} />)
   }
 }
 
-const Message = ({name, content}) => <div><strong>{name}:</strong> {content}</div>;
-
-const messages = [
-  {name: "Richard", content: "do you like hurting other people?"},
-]
-
 class InputField extends React.Component {
+  componentDidMount() {
+    console.log('input');
+  }
+  
   render() {
     return <input type="text" placeholder="клац клац"/>
   }
 }
 
 class SendButton extends React.Component {
-  constructor() {
-    super();
-    // this.state = {};      
-    this.answerRichard = this.answerRichard.bind(this);      
-    console.log("constructor");
-  }
-
   answerRichard() {
-    const jackRep = {name: "Jacket", content: "*BRUH*"};
-    // const chat = messages.push({name: "Jacket", content: "*BRUH*"});
-    this.setState();
-    
-    console.log(messages);
+    console.log('4pok');
+    return messages.push({name: "Jacket", content: "..."})
   }
 
-  render() {
-    return (
-      <button onClick={this.answerRichard}>
-        click
-      </button>
-    );
-  }
+ componentDidMount() {
+  console.log('send');
 }
 
-ReactDom.render(
-  <>
-    <MessageField messages={messages} />
-    <InputField />
-    <SendButton />
-  </>,
-  document.getElementById("root")
-);
+ render() {
+   return (
+     <button onClick={this.answerRichard}>
+       click
+     </button>
+   );
+ }
+}
 
-//console.log('4pok'); !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <MessageField messages={messages}/>
+      <InputField />
+      <SendButton />
+    </div>
+    )
+  }
+} 
+
+ReactDom.render(<App />,document.getElementById("root"));
+
+//console.log('4pok'); 
+
+// компоненты с логикой - умные, контейнеры(только логкиа)
+// компоненты с версткой - глупые, просто компоненты, не содежат стейта
